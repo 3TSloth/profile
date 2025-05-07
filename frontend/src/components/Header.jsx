c32bc2c7c02dff39fc970dcdfb588d9c9a4f7f97
3TSloth <pdevsloth3@gmail.com>
2025-05-06 23:15:00 -0400

    Fixed unwanted border line

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header({ setActiveContent }) {
   return (
     <div id="header">
       <div className="grid grid-cols-[200px_1fr]">
-        <div className="col-start-1 col-end-2 border-b-2 border-cyan-500">
+        <div className="col-start-1 col-end-2 border-b-2 border-r-2 sm:border-r-0 border-cyan-500">
           <Logo></Logo>
         </div>
         <div className="sm:visible invisible col-start-2 border-r-2 border-b-2 border-cyan-500">
